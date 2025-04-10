1fd3b0a679817ebd8aa34022edad2ba07cfc1a0e
Volodymyr <biletskiy1780@gmail.com>
2025-04-10 15:30:26 +0300

    fix: update base path in vite.config.ts for consistency

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/JS-BandStore-react-typescript-mui-redux/',
+  base: '/JS-BandStore-react-redux-typescript-mui',
   plugins: [react()],
 });
